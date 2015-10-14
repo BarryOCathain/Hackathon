8a106ef6ddf7a0efd90461b6919ace9643db4738
arcangel666 <arcangel666@hotmail.co.uk>
2015-10-14 13:00:29 +0100

    EF added

--- PWSHackathonWCF/PWSService.svc.cs
+++ PWSHackathonWCF/PWSService.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using PWSHackathonDAL;
 
 namespace PWSHackathonWCF
 {
@@ -12,8 +13,19 @@ namespace PWSHackathonWCF
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class PWSService : IAddressService
     {
+        PWS_DatabaseEntities _db = new PWS_DatabaseEntities();
+
         public Address CreateAddress(Address address)
         {
+            PWSHackathonDAL.Address dbAddress = new PWSHackathonDAL.Address();
+            dbAddress.AddressLine1 = address.Line1;
+            dbAddress.AddressLine2 = address.Line2;
+           
+
+            _db.Addresses.Add(dbAddress);
+
+            _db.SaveChanges();
+
             Address ret = new Address();
 
 
@@ -46,6 +58,28 @@ namespace PWSHackathonWCF
 
         public Address UpdateAddress(Address address)
         {
+            string wcfPostcode = address.PostCode;
+
+            PWSHackathonDAL.Address tempAddress = _db.Addresses
+                .Where(a => a.Postcode == wcfPostcode)
+                .FirstOrDefault();
+
+            if(tempAddress != null)
+            {
+                tempAddress.AddressLine1 = address.Line1;
+                tempAddress.AddressLine2 = address.Line1;
+                tempAddress.AddressLine3 = address.Line3;
+                tempAddress.AddressLine4 = address.Line4;
+                tempAddress.Email = address.EMail;
+                tempAddress.Telephone = address.TelephoneNumber;
+
+                _db.SaveChanges();
+
+                return tempAddress;
+
+            }
+
+
             Address ret = new Address();
 
             return ret;
