3b9e0393f0dfe1102c697485d4730e5472907f65
emyi146 <manuel.garc.garc@gmail.com>
2015-10-14 16:03:22 +0100

    Added missing file

---
+++ PWSHackathonWCF/Services/IAddressService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace PWSHackathonWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAddressService
+    {
+
+        [OperationContract]
+        Address GetAddress(string postcode);
+
+        [OperationContract]
+        Address CreateAddress(Address address);
+
+        [OperationContract]
+        Address DeleteAddress(Address address);
+
+        [OperationContract]
+        Address UpdateAddress(Address address);
+
+        [OperationContract]
+        List<Address> GetAllAddresses();
+
+    }
+
+
+ 
+}
