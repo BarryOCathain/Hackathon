4deef914fb05bd11fff77ff8f2057bce846d2fb1
pmckillen <pat_mckillen@outlook.com>
2015-10-15 09:14:10 +0100

    Adding Start Page

---
+++ PWSHackathonClient/StartPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PWSHackathonClient
+{
+    /// <summary>
+    /// Interaction logic for RiskWindow.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
